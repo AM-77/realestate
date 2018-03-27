cb22b09f84a73913b9e9c67c26ad3f7e45989957
AM77 <amine.griche77@gmail.com>
2018-03-27 20:48:15 +0200

    statitcs details: added number of visites stats.

--- src/main/java/com/realestate/models/Statistcs_details.java
+++ src/main/java/com/realestate/models/Statistcs_details.java
@@ -18,6 +18,27 @@ public class Statistcs_details {
 	private Map<String, Integer> nbr_all_visites_website_by_operators_mounths;
 	private Map<String, Double> percent_all_visites_website_by_operators_mounths;
 	
+	private int nbr_appoi_locale_one;
+	private int nbr_appoi_locale_two;
+	private int nbr_appoi_locale_three;
+	private int nbr_appoi_locale_four;
+	private int nbr_appoi_locale_five;
+	private double percent_appoi_locale_one;
+	private double percent_appoi_locale_two;
+	private double percent_appoi_locale_three;
+	private double percent_appoi_locale_four;
+	private double percent_appoi_locale_five;
+	private int nbr_appoi_F1;
+	private int nbr_appoi_F2;
+	private int nbr_appoi_F3;
+	private int nbr_appoi_F4;
+	private int nbr_appoi_F5;
+	private double percent_appoi_F1;
+	private double percent_appoi_F2;
+	private double percent_appoi_F3;
+	private double percent_appoi_F4;
+	private double percent_appoi_F5;
+	
 	
 	
 	public int getNbr_all_website_visites() {
@@ -128,6 +149,166 @@ public class Statistcs_details {
 	public void setPercent_all_visites_website_by_operators_mounths(
 			Map<String, Double> percent_all_visites_website_by_operators_mounths) {
 		this.percent_all_visites_website_by_operators_mounths = percent_all_visites_website_by_operators_mounths;
+	}
+	
+	public int getNbr_appoi_locale_one() {
+		return nbr_appoi_locale_one;
+	}
+
+	public void setNbr_appoi_locale_one(int nbr_appoi_locale_one) {
+		this.nbr_appoi_locale_one = nbr_appoi_locale_one;
+	}
+
+	public int getNbr_appoi_locale_two() {
+		return nbr_appoi_locale_two;
+	}
+
+	public void setNbr_appoi_locale_two(int nbr_appoi_locale_two) {
+		this.nbr_appoi_locale_two = nbr_appoi_locale_two;
+	}
+
+	public int getNbr_appoi_locale_three() {
+		return nbr_appoi_locale_three;
+	}
+
+	public void setNbr_appoi_locale_three(int nbr_appoi_locale_three) {
+		this.nbr_appoi_locale_three = nbr_appoi_locale_three;
+	}
+
+	public int getNbr_appoi_locale_four() {
+		return nbr_appoi_locale_four;
+	}
+
+	public void setNbr_appoi_locale_four(int nbr_appoi_locale_four) {
+		this.nbr_appoi_locale_four = nbr_appoi_locale_four;
+	}
+
+	public int getNbr_appoi_locale_five() {
+		return nbr_appoi_locale_five;
+	}
+
+	public void setNbr_appoi_locale_five(int nbr_appoi_locale_five) {
+		this.nbr_appoi_locale_five = nbr_appoi_locale_five;
+	}
+
+	public double getPercent_appoi_locale_one() {
+		return percent_appoi_locale_one;
+	}
+
+	public void setPercent_appoi_locale_one(double percent_appoi_locale_one) {
+		this.percent_appoi_locale_one = percent_appoi_locale_one;
+	}
+
+	public double getPercent_appoi_locale_two() {
+		return percent_appoi_locale_two;
+	}
+
+	public void setPercent_appoi_locale_two(double percent_appoi_locale_two) {
+		this.percent_appoi_locale_two = percent_appoi_locale_two;
+	}
+
+	public double getPercent_appoi_locale_three() {
+		return percent_appoi_locale_three;
+	}
+
+	public void setPercent_appoi_locale_three(double percent_appoi_locale_three) {
+		this.percent_appoi_locale_three = percent_appoi_locale_three;
+	}
+
+	public double getPercent_appoi_locale_four() {
+		return percent_appoi_locale_four;
+	}
+
+	public void setPercent_appoi_locale_four(double percent_appoi_locale_four) {
+		this.percent_appoi_locale_four = percent_appoi_locale_four;
+	}
+
+	public double getPercent_appoi_locale_five() {
+		return percent_appoi_locale_five;
+	}
+
+	public void setPercent_appoi_locale_five(double percent_appoi_locale_five) {
+		this.percent_appoi_locale_five = percent_appoi_locale_five;
+	}
+
+	public int getNbr_appoi_F1() {
+		return nbr_appoi_F1;
+	}
+
+	public void setNbr_appoi_F1(int nbr_appoi_F1) {
+		this.nbr_appoi_F1 = nbr_appoi_F1;
+	}
+
+	public int getNbr_appoi_F2() {
+		return nbr_appoi_F2;
+	}
+
+	public void setNbr_appoi_F2(int nbr_appoi_F2) {
+		this.nbr_appoi_F2 = nbr_appoi_F2;
+	}
+
+	public int getNbr_appoi_F3() {
+		return nbr_appoi_F3;
+	}
+
+	public void setNbr_appoi_F3(int nbr_appoi_F3) {
+		this.nbr_appoi_F3 = nbr_appoi_F3;
+	}
+
+	public int getNbr_appoi_F4() {
+		return nbr_appoi_F4;
+	}
+
+	public void setNbr_appoi_F4(int nbr_appoi_F4) {
+		this.nbr_appoi_F4 = nbr_appoi_F4;
+	}
+
+	public int getNbr_appoi_F5() {
+		return nbr_appoi_F5;
+	}
+
+	public void setNbr_appoi_F5(int nbr_appoi_F5) {
+		this.nbr_appoi_F5 = nbr_appoi_F5;
+	}
+
+	public double getPercent_appoi_F1() {
+		return percent_appoi_F1;
+	}
+
+	public void setPercent_appoi_F1(double percent_appoi_F1) {
+		this.percent_appoi_F1 = percent_appoi_F1;
+	}
+
+	public double getPercent_appoi_F2() {
+		return percent_appoi_F2;
+	}
+
+	public void setPercent_appoi_F2(double percent_appoi_F2) {
+		this.percent_appoi_F2 = percent_appoi_F2;
+	}
+
+	public double getPercent_appoi_F3() {
+		return percent_appoi_F3;
+	}
+
+	public void setPercent_appoi_F3(double percent_appoi_F3) {
+		this.percent_appoi_F3 = percent_appoi_F3;
+	}
+
+	public double getPercent_appoi_F4() {
+		return percent_appoi_F4;
+	}
+
+	public void setPercent_appoi_F4(double percent_appoi_F4) {
+		this.percent_appoi_F4 = percent_appoi_F4;
+	}
+
+	public double getPercent_appoi_F5() {
+		return percent_appoi_F5;
+	}
+
+	public void setPercent_appoi_F5(double percent_appoi_F5) {
+		this.percent_appoi_F5 = percent_appoi_F5;
 	}
 
 	
