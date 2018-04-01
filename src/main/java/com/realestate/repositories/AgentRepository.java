a103649d19311462143cf902d53eb80d7e42893c
AM77 <amine.griche77@gmail.com>
2018-04-01 22:59:40 +0200

    agent repo: added subscribe method

--- src/main/java/com/realestate/repositories/AgentRepository.java
+++ src/main/java/com/realestate/repositories/AgentRepository.java
@@ -15,5 +15,13 @@ import com.realestate.models.Agent;
 
 public interface AgentRepository extends JpaRepository<Agent, Integer>{
 
-	
+	@Transactional
+	@Modifying(clearAutomatically = true)
+	@Query(value = "INSERT INTO `agent`VALUES ( :id, :email, :password, :username, :name, :last_name, :birthdate, :gender, :blocked, :locale, "
+					+ ":profile_pic, :cv, :phone, :key_confirm)", nativeQuery= true)
+	public int agent_subscribe(@Param("id")int id, @Param("email")String email, @Param("gender")String gender, @Param("birthdate")Date birthdate, 
+								@Param("blocked")int blocked, @Param("name")String name, @Param("password")String password, 
+								@Param("last_name")String last_name, @Param("profile_pic")String profile_pic, @Param("phone")String phone, 
+								@Param("username")String username, @Param("cv")String cv, @Param("locale")String locale,  @Param("key_confirm")String key_confirm);
+
 }
