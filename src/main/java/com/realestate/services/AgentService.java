36179feaecb7548f67e3e827f507326a56958e1c
AM77 <amine.griche77@gmail.com>
2018-04-01 23:05:46 +0200

    created agent service

---
+++ src/main/java/com/realestate/services/AgentService.java
@@ -0,0 +1,15 @@
+package com.realestate.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.realestate.models.Agent;
+import com.realestate.repositories.AgentRepository;
+
+@Service
+public class AgentService {
+	
+	
+}
