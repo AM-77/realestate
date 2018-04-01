2935b3cff1dfa5933d87270d4d05308b5849e80e
AM77 <amine.griche77@gmail.com>
2018-04-01 22:58:54 +0200

    created agent repo

---
+++ src/main/java/com/realestate/repositories/AgentRepository.java
@@ -0,0 +1,19 @@
+package com.realestate.repositories;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.realestate.models.Agent;
+
+
+public interface AgentRepository extends JpaRepository<Agent, Integer>{
+
+	
+}
