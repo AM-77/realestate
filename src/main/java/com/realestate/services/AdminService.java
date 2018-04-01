d3980a018a8aa11f39041a76d191e917baed572d
AM77 <amine.griche77@gmail.com>
2018-04-01 22:51:36 +0200

    added admin subscribe method

--- src/main/java/com/realestate/services/AdminService.java
+++ src/main/java/com/realestate/services/AdminService.java
@@ -12,6 +12,12 @@ public class AdminService {
 	@Autowired
 	private AdminRepository adminRepository; 
 
-	
+	public boolean admin_subscribe(Admin admin) {
+		if(adminRepository.admin_subscribe(admin.getId(), admin.getEmail(), admin.getPhone(), admin.getName(), 
+				admin.getPassword(), admin.getLast_name(),admin.getProfile_pic())==1)
+			return true;
+		else
+		return false;
+	}
 
 }
