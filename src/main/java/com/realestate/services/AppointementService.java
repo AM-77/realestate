3ed43787747387c212575b2b19a37a8a7bafc178
AM77 <amine.griche77@gmail.com>
2018-04-02 17:19:34 +0200

    added nbr_appointement_* methods

--- src/main/java/com/realestate/services/AppointementService.java
+++ src/main/java/com/realestate/services/AppointementService.java
@@ -51,6 +51,113 @@ public class AppointementService {
 		} 
 	}
 
+	public int add(Date date, int first_half, int second_half, int id_lodgement, int id_agent, int id_client) {
+		return appointementRepository.add(date, first_half, second_half, id_lodgement, id_agent, id_client);
+	}
 	
+	
+	public List<Appointement> get_Appointements_by_id_lodgement(int id_lodgement){
+		return appointementRepository.get_Appointements_by_id_lodgement(id_lodgement);
+	}
+
+	public List<Appointement> get_Appointements_by_id_client(int id) {
+		return appointementRepository.get_Appointements_by_id_client(id);
+	}
+
+	public boolean is_avail_lodgement_by_id_date_first_half(Date date, int id_lodg) {
+		
+		if(appointementRepository.is_avail_lodgement_by_id_date_first_half(date, id_lodg) == null)
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean is_avail_lodgement_by_id_date_second_half(Date date, int id_lodg) {
+		
+		if(appointementRepository.is_avail_lodgement_by_id_date_second_half(date, id_lodg) == null)
+			return true;
+		else
+			return false;
+	}
+
+	public boolean existe_appoi_by_id_client(int id, int id_appoi) {
+		if(appointementRepository.existe_appoi_by_id_client(id, id_appoi) == null)
+			return false;
+		else
+			return true;
+	}
+
+	public Appointement get_appointements_by_id(int id_appointement) {
+		return appointementRepository.get_appointements_by_id(id_appointement);
+	}
+
+	public boolean client_confirm_appointement(int id_appointement) {
+	
+		if(appointementRepository.client_confirm_appointement( id_appointement) == 1){
+			return true;
+		}else {
+			return false;
+		}
+	
+	}
+
+	public boolean client_cancel_appointement(int id_appointement) {
+		if(appointementRepository.client_cancel_appointement( id_appointement) == 1){
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	public int get_canceled_value(int id_appointement) {
+		return appointementRepository.get_canceled_value(id_appointement);
+	}
+
+	public List<Appointement> get_Appointements_by_id_agent(int id_agent) {
+		return appointementRepository. get_Appointements_by_id_agent(id_agent);
+	}
+
+	public boolean agent_cancel_appointement(int id_appointement) {
+		if(appointementRepository.agent_cancel_appointement(id_appointement) == 1){
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	public boolean agent_confirm_appointement(int id_appointement) {
+		if(appointementRepository.agent_confirm_appointement(id_appointement) == 1){
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	public boolean save_review(int id_appointement, String review) {
+		if(appointementRepository.save_review(id_appointement, review) == 1){
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	public boolean change_review(int id_appointement, String review) {
+		if(appointementRepository.change_review(id_appointement, review) == 1){
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public boolean existe_appoi_by_id_agent(int id, int id_appoi) {
+		if(appointementRepository.existe_appoi_by_id_agent(id, id_appoi) == null)
+			return false;
+		else
+			return true;
+	}
+
+	public void sold_cancel_appointement(int id) {
+		appointementRepository.sold_cancel_appointement(id);
+	}
 
 }
