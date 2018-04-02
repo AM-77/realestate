591707b93d8a627bb402d7a0aff7c11f6a366c33
AM77 <amine.griche77@gmail.com>
2018-04-02 17:17:05 +0200

    added is_avil_* methods

--- src/main/java/com/realestate/repositories/AppointementRepository.java
+++ src/main/java/com/realestate/repositories/AppointementRepository.java
@@ -13,5 +13,17 @@ import org.springframework.data.repository.query.Param;
 import com.realestate.models.Appointement;
 
 public interface AppointementRepository extends JpaRepository<Appointement, Integer> {
+	
+	@Query(value="SELECT * FROM appointement WHERE date=:date AND first_half='1' AND id_lodgement=:id_lodgement AND canceled='0'  LIMIT 1", nativeQuery=true)
+	public Appointement is_avail_date_first_half(@Param("date") Date date, @Param("id_lodgement") int id_lodgement);
+	
+	@Query(value="SELECT * FROM appointement WHERE date=:date AND second_half='1' AND id_lodgement=:id_lodgement AND canceled='0'  LIMIT 1", nativeQuery=true)
+	public Appointement is_avail_date_second_half(@Param("date") Date date, @Param("id_lodgement") int id_lodgement);
 
+	@Query(value="SELECT * FROM appointement WHERE date=:date AND id_agent=:id_agent AND first_half='1' AND canceled='0' LIMIT 1", nativeQuery=true)
+	public Appointement is_avail_agent_first_half(@Param("date") Date date, @Param("id_agent") int id_agent);
+	
+	@Query(value="SELECT * FROM appointement WHERE date=:date AND id_agent=:id_agent AND second_half='1' AND canceled='0'  LIMIT 1", nativeQuery=true)
+	public Appointement is_avail_agent_second_half(@Param("date") Date date, @Param("id_agent") int id_gent);
+	
 }
