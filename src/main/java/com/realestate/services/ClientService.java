275ff57cc290f7a77f255cebfd063fb507431173
AM77 <amine.griche77@gmail.com>
2018-04-05 00:07:16 +0200

    added subscribe client

--- src/main/java/com/realestate/services/ClientService.java
+++ src/main/java/com/realestate/services/ClientService.java
@@ -9,5 +9,27 @@ import com.realestate.repositories.ClientRepository;
 @Service
 public class ClientService {
 	
+	@Autowired
+	private ClientRepository clientRepository; 
 	
+	public boolean client_subscribe(Client client) {
+		if(clientRepository.client_subscribe(client.getId(), 
+											 client.getEmail(), 
+											 client.getGender(), 
+											 client.getBirthdate(), 
+											 client.getBlocked(), 
+											 client.getName(), 
+											 client.getPassword(), 
+											 client.getLast_name(), 
+											 client.getProfile_pic(), 
+											 client.getPhone(), 
+											 client.getUsername(),
+											 client.getConfirm_key())==1)
+				return true;
+		else
+			return false;
+	}
+	
+	
+
 }
