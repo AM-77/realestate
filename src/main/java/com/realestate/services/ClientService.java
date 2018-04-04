93cd5d131434fab058218bf7c9afd149eb141bec
AM77 <amine.griche77@gmail.com>
2018-04-05 00:06:26 +0200

    created client servise

---
+++ src/main/java/com/realestate/services/ClientService.java
@@ -0,0 +1,13 @@
+package com.realestate.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.realestate.models.Client;
+import com.realestate.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+	
+	
+}
