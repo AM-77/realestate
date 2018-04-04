b89bdddf1a95db460066f836ffcd02a53f4899df
AM77 <amine.griche77@gmail.com>
2018-04-04 23:11:08 +0200

    added subscribe client

--- src/main/java/com/realestate/repositories/ClientRepository.java
+++ src/main/java/com/realestate/repositories/ClientRepository.java
@@ -14,5 +14,22 @@ import com.realestate.models.Client;
 
 public interface ClientRepository extends JpaRepository<Client, Integer>{
 
+	@Transactional
+	@Modifying(clearAutomatically = true)
+	@Query(value = "INSERT INTO `client`VALUES (:id, :email, :password, :username, :name, :last_name, :birthdate, :gender, :phone, "
+					+ ":blocked, :profile_pic, :key_confirm)", nativeQuery= true)
+	public int client_subscribe(@Param("id")int id,
+								 @Param("email") String email,
+								 @Param("gender") String gender,
+								 @Param("birthdate") Date birthdate, 
+								 @Param("blocked") int blocked, 
+								 @Param("name") String name, 
+								 @Param("password") String password, 
+								 @Param("last_name") String lastname, 
+								 @Param("profile_pic") String profile_pic, 
+								 @Param("phone") String phone, 
+								 @Param("username") String username,
+								 @Param("key_confirm") String key_confirm);
+
 	
 }
