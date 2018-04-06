2191831796dc6360b7648daa0f2e710a27a3fc99
AM77 <amine.griche77@gmail.com>
2018-04-06 15:17:57 +0200

    created favorite repo

---
+++ src/main/java/com/realestate/repositories/FavoriteRepository.java
@@ -0,0 +1,16 @@
+package com.realestate.repositories;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.realestate.models.Favorite;
+
+public interface FavoriteRepository extends JpaRepository<Favorite, Integer>{
+	
+}
