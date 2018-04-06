7616b0b0dbf29abe4bdb4bba9957daa2bc924ea3
AM77 <amine.griche77@gmail.com>
2018-04-06 15:19:01 +0200

    created favorite service

---
+++ src/main/java/com/realestate/services/FavoriteService.java
@@ -0,0 +1,15 @@
+package com.realestate.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.realestate.models.Favorite;
+import com.realestate.repositories.FavoriteRepository;
+
+@Service
+public class FavoriteService {
+	
+	
+}
