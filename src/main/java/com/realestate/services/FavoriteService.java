7d1e0f5872f94d4e1f84fd2ad25a2c557f568ac0
AM77 <amine.griche77@gmail.com>
2018-04-06 15:19:30 +0200

    added favorite methodes

--- src/main/java/com/realestate/services/FavoriteService.java
+++ src/main/java/com/realestate/services/FavoriteService.java
@@ -11,5 +11,43 @@ import com.realestate.repositories.FavoriteRepository;
 @Service
 public class FavoriteService {
 	
+	@Autowired
+	private FavoriteRepository favoriteRepository; 
+
+
+	public boolean add_favorite(int id_lodgement, int id_client) {
+		if(favoriteRepository.add_favorite(id_lodgement, id_client) == 1)
+			return true;
+		else
+			return false;
+	}
 	
+	public boolean existe_favorites_by_client_and_lodgement(int id_client, int id_lodgement) {
+		
+		if(favoriteRepository.existe_favorites_by_client_and_lodgement(id_client, id_lodgement) == null)
+			return false;
+		else
+			return true;
+		
+	}
+	
+	public boolean existe_favorites_by_client_and_favorite(int id_client, int id_fav) {
+		
+		if(favoriteRepository.existe_favorites_by_client_and_favorite(id_client, id_fav) == null)
+			return false;
+		else
+			return true;
+		
+	}
+	
+	public List<Favorite> get_favorites_by_client(int id_client){
+		return favoriteRepository.get_favorites_by_client(id_client);
+	}
+
+	public boolean remove_favorite(int id_fav) {
+		if(favoriteRepository.remove_favorite(id_fav) == 1)
+			return true;
+		else
+			return false;
+	}
 }
