de1e3667f76884b3dcc35b2e7fa45288ec1d8403
AM77 <amine.griche77@gmail.com>
2018-04-12 20:53:47 +0200

    created operator service

---
+++ src/main/java/com/realestate/services/OperatorService.java
@@ -0,0 +1,14 @@
+package com.realestate.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.realestate.models.Operator;
+import com.realestate.repositories.OperatorRepository;
+
+@Service
+public class OperatorService {
+
+}
