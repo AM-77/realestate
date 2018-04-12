ac74e3c563c97252fe788ebf59242248b6f0586f
AM77 <amine.griche77@gmail.com>
2018-04-12 20:50:38 +0200

    operator repo: added subscribe op

--- src/main/java/com/realestate/repositories/OperatorRepository.java
+++ src/main/java/com/realestate/repositories/OperatorRepository.java
@@ -25,58 +25,5 @@ public interface OperatorRepository extends JpaRepository<Operator, Integer>{
 								@Param("username")String username, @Param("cv")String cv, @Param("locale")String locale, @Param("key_confirm")String key_confirm);
 
 	
-	@Query(value="SELECT * FROM operator WHERE email= :email  LIMIT 1", nativeQuery=true)
-	public Operator get_operator_by_email(@Param("email") String email);
-
-	@Query(value="SELECT * FROM operator WHERE email= :email AND password= :password  LIMIT 1", nativeQuery=true)
-	public Operator get_operator_by_email_and_password(@Param("email")String email, @Param("password")String password);
-
-
-	@Query(value="SELECT * FROM operator WHERE id= :id  LIMIT 1", nativeQuery=true)
-	public Operator get_operator_by_id(@Param("id")int user_id);
-
-	@Query(value="SELECT COUNT(*) FROM operator ", nativeQuery=true)
-	public int nbr_account();
-
-	@Query(value="SELECT * FROM operator WHERE blocked = 2 ", nativeQuery=true)
-	public List<Operator> get_operator_subscribe_demand();
-
-	@Transactional
-	@Modifying(clearAutomatically = true)
-	@Query(value="DELETE FROM `operator` WHERE `id` = :id ", nativeQuery=true)
-	public int remove_an_op(@Param("id")int id);
-
-	
-	@Transactional
-	@Modifying(clearAutomatically = true)
-	@Query(value="UPDATE `operator` SET `blocked` = '0' WHERE `id` = :id ;", nativeQuery=true)
-	public int confirm_an_op(@Param("id")int id);
-
-
-	@Transactional
-	@Modifying(clearAutomatically = true)
-	@Query(value="UPDATE `operator` SET `blocked` = '1' WHERE `email` = :email ;", nativeQuery=true)
-	public int block_an_op(@Param("email")String email);
-
-	@Transactional
-	@Modifying(clearAutomatically = true)
-	@Query(value="UPDATE `operator` SET `blocked` = '0' WHERE `email` = :email ;", nativeQuery=true)
-	public int deblock_an_op(@Param("email")String email);
-
-
-	@Transactional
-	@Modifying(clearAutomatically = true)
-	@Query(value="DELETE FROM `operator` WHERE `email` = :email ", nativeQuery=true)
-	public int remove_an_op(@Param("email")String email);
-
-
-	@Query(value="SELECT * FROM operator WHERE confirm_key= :confirm_key  LIMIT 1", nativeQuery=true)
-	public Operator get_operator_by_confirmation_key(@Param("confirm_key") String confirm_key);
-
-
-	@Transactional
-	@Modifying(clearAutomatically = true)
-	@Query(value="UPDATE `operator` SET `email` = :email , `username` = :username, `name` = :name, `last_name` = :last_name, `phone` = :phone, `phone` = :birthdate WHERE id = :id  ", nativeQuery=true)
-	public int update_operator_profile(@Param("id")int id, @Param("username")String username, @Param("name")String name, @Param("last_name")String last_name, @Param("email")String email, @Param("phone")String phone, @Param("birthdate")Date birthdate);
 	
 }
