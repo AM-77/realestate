fcb2e5ba5b8f22e9db47671ff2089fc707e22afa
AM77 <amine.griche77@gmail.com>
2018-04-13 21:12:39 +0200

    lodgement repo: added add/remove lodgement and search wich methods

--- src/main/java/com/realestate/repositories/LodgementRepository.java
+++ src/main/java/com/realestate/repositories/LodgementRepository.java
@@ -119,6 +119,40 @@ public interface LodgementRepository extends JpaRepository<Lodgement, Integer>{
 																		@Param("type")String type,
 																		@Param("floor")int floor);
 
+	@Query(value="SELECT * FROM lodgement WHERE surface BETWEEN :min_surface AND :max_surface "
+																		 + " AND floor= :floor "
+																	     + " AND price BETWEEN :min_price "
+																	     + " AND :max_price "
+																	     + "AND sold = 0 ", nativeQuery=true)
+	public List<Lodgement> get_lodgements_by_price_and_surface_and_floor(@Param("min_price")double min_price, 
+																@Param("max_price")double max_price, 
+																@Param("min_surface")double min_surface,
+																@Param("max_surface")double max_surface,
+																@Param("floor")int floor);
 	
+	
+	
+	@Query(value = "SELECT * FROM lodgement WHERE ( address = :address ) AND ( type = :type ) AND  ( surface BETWEEN :min_surface AND :max_surface ) AND ( price BETWEEN :min_price AND :max_price ) AND ( floor BETWEEN :min_floor AND :max_floor ) AND sold = 0  LIMIT 1", nativeQuery= true)
+	public Lodgement search_wish(@Param("address") String address, 
+								  @Param("type") String type, 
+								  @Param("max_surface") double max_surface, 
+								  @Param("min_surface") double min_surface, 
+								  @Param("max_price") double max_price,
+								  @Param("min_price") double min_price, 
+								  @Param("max_floor") int max_floor, 
+								  @Param("min_floor") int min_floor);
+
+	@Transactional
+	@Modifying(clearAutomatically = true)
+	@Query(value = "INSERT INTO `lodgement`  VALUES (NULL, :locale, :address , :surface, :type, :floor, :price, :pics, '0')", nativeQuery= true)
+	public int add_lodgement(@Param("locale")int locale, @Param("address")String address, @Param("type")String type, @Param("price")double price, @Param("surface")double surface, @Param("floor")int floor, @Param("pics")String pics);
+
+	@Query(value="SELECT max(id) FROM lodgement", nativeQuery=true)
+	public int getLastId();
+
+	@Transactional
+	@Modifying(clearAutomatically = true)
+	@Query(value = "UPDATE `lodgement` SET  sold = 1  WHERE id = :id ", nativeQuery= true)
+	public int remove_lodgement(@Param("id")int id);
 	
 }
