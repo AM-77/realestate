ec62e0037837a0ee4000a4f17d373f765924d28b
AM77 <amine.griche77@gmail.com>
2018-04-13 21:15:10 +0200

    created lodgement servise

---
+++ src/main/java/com/realestate/services/LodgementService.java
@@ -0,0 +1,17 @@
+package com.realestate.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.realestate.models.Lodgement;
+import com.realestate.repositories.LodgementRepository;
+
+@Service
+public class LodgementService {
+	
+	
+
+	
+}
