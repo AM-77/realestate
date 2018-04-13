03daa0e4632403f29efa2b81102db15d96963d9c
AM77 <amine.griche77@gmail.com>
2018-04-13 21:11:30 +0200

    lodgement repo: a get_by_* method

--- src/main/java/com/realestate/repositories/LodgementRepository.java
+++ src/main/java/com/realestate/repositories/LodgementRepository.java
@@ -42,5 +42,18 @@ public interface LodgementRepository extends JpaRepository<Lodgement, Integer>{
 																		@Param("min_floor")int min_floor,
 																		@Param("max_floor")int max_floor);
 
+	@Query(value="SELECT * FROM lodgement WHERE surface BETWEEN :min_surface AND :max_surface "
+												+ "AND type= :type "
+												+ "AND price BETWEEN :min_price AND :max_price "
+												+ "AND floor BETWEEN :min_floor AND :max_floor "
+												+ "AND sold = 0 ", nativeQuery=true)
+	public List<Lodgement> get_lodgements_by_price_surface_type_and_floor(	@Param("min_price")double min_price, 
+																		@Param("max_price")double max_price,
+																		@Param("min_surface")double min_surface, 
+																		@Param("max_surface")double max_surface, 
+																		@Param("min_floor")int min_floor,
+																		@Param("max_floor")int max_floor,
+																		@Param("type")String type);
+
 	
 }
