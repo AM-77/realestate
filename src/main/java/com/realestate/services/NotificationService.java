07e1b10c3e9105f8c8f4478390690a583eb43e93
AM77 <amine.griche77@gmail.com>
2018-04-14 19:28:05 +0200

    notification service : added get_notifications

--- src/main/java/com/realestate/services/NotificationService.java
+++ src/main/java/com/realestate/services/NotificationService.java
@@ -87,4 +87,131 @@ public class NotificationService {
 		notificationRepository.set_viewed_notifications_for_client(id_client);
 	}
 	
+	public void add_sold_notification(int id, String client_notif, String agent_notif) {
+		notificationRepository.add_sold_notification( id, client_notif, agent_notif);
+	}	
+	
+	public List<Notification_details> get_notifications(HttpSession session) throws ParseException{
+		
+		List<Notification_details> list_notification_details = new ArrayList<Notification_details>();
+		
+		if(session.getAttribute("client") != null) {
+			
+			List<Notification> notifs = get_notifications_by_client_id(((Client)session.getAttribute("client")).getId());
+			
+			Lodgement lodgement; 
+			Client client; 
+			Agent agent;
+			Appointement appointement;
+			
+			for(Notification notif : notifs) {
+							
+				if(notif.getClient_notif().equals("no_notif"))
+					continue;
+				
+				lodgement = lodgementService.get_lodgement_by_appointement_all_id(notif.getId_appointement());
+				client = clientService.get_client_by_appointement_id(notif.getId_appointement());
+				agent = agentService.get_agent_by_appointement_id(notif.getId_appointement());
+				appointement = appointementService.get_appointements_by_id(notif.getId_appointement());
+				
+				String appointement_time, agent_confirm = "Not confirmed", client_confirm = "Not confirmed";
+				
+				if(appointement.getFirst_half() == 1) {
+					appointement_time = "Morning";
+				}else {
+					appointement_time = "Evening";
+				}
+				
+				if(appointement.getAgent_confirm() == 1) {
+					agent_confirm = "Confirmed";
+				}
+				
+				if(appointement.getClient_confirm() == 1) {
+					client_confirm = "Confirmed";
+				}
+				
+
+				list_notification_details.add(new Notification_details( notif.getId(), 
+																		 notif.getClient_notif(), 
+																	     notif.getTime(), 
+																	     appointement.getDate(), 
+																	     appointement_time, 
+																	     agent_confirm, 
+																	     client_confirm, 
+																	     client.getId(), 
+																	     client.getUsername(), 
+																	     lodgement.getId(), 
+																	     lodgement.getType(), 
+																	     lodgement.getAddress(), 
+																	     agent.getId(), 
+																	     agent.getUsername(),(notif.getV_client() == 1)));
+				
+			}				
+				
+		}else {
+			
+			if(session.getAttribute("agent") != null) {
+				
+				System.out.println(((Agent)session.getAttribute("agent")).getId());
+				
+				List<Notification> notifs = get_notifications_by_agent_id(((Agent)session.getAttribute("agent")).getId());
+				
+				Lodgement lodgement; 
+				Client client; 
+				Agent agent;
+				Appointement appointement;
+				
+				for(Notification notif : notifs) {
+								
+					if(notif.getAgent_notif().equals("no_notif"))
+						continue;
+					
+					lodgement = lodgementService.get_lodgement_by_appointement_all_id(notif.getId_appointement());
+					client = clientService.get_client_by_appointement_id(notif.getId_appointement());
+					agent = agentService.get_agent_by_appointement_id(notif.getId_appointement());
+					appointement = appointementService.get_appointements_by_id(notif.getId_appointement());
+					
+					String appointement_time, agent_confirm = "Not confirmed", client_confirm = "Not confirmed";
+					
+					if(appointement.getFirst_half() == 1) {
+						appointement_time = "Morning";
+					}else {
+						appointement_time = "Evening";
+					}
+					
+					if(appointement.getAgent_confirm() == 1) {
+						agent_confirm = "Confirmed";
+					}
+					
+					if(appointement.getClient_confirm() == 1) {
+						client_confirm = "Confirmed";
+					}
+					
+
+					list_notification_details.add(new Notification_details( notif.getId(), 
+																			 notif.getClient_notif(), 
+																		     notif.getTime(), 
+																		     appointement.getDate(), 
+																		     appointement_time, 
+																		     agent_confirm, 
+																		     client_confirm, 
+																		     client.getId(), 
+																		     client.getUsername(), 
+																		     lodgement.getId(), 
+																		     lodgement.getType(), 
+																		     lodgement.getAddress(), 
+																		     agent.getId(), 
+																		     agent.getUsername(),
+																		     (notif.getV_client() == 1)));
+					
+				}				
+					
+			}
+			
+		}
+							
+		return list_notification_details;
+		
+	}
+
 }
