859121357b7936f5833e9262bbc261eb049d3140
AM77 <amine.griche77@gmail.com>
2018-04-14 19:24:18 +0200

    created notification service

---
+++ src/main/java/com/realestate/services/NotificationService.java
@@ -0,0 +1,23 @@
+package com.realestate.services;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.realestate.models.Agent;
+import com.realestate.models.Appointement;
+import com.realestate.models.Client;
+import com.realestate.models.Lodgement;
+import com.realestate.models.Notification;
+import com.realestate.models.Notification_details;
+import com.realestate.repositories.NotificationRepository;
+
+@Service
+public class NotificationService {
+	
+}
