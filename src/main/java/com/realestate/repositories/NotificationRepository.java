e0b98ced609d5c0d253d77fab5a7e947180e7ad3
AM77 <amine.griche77@gmail.com>
2018-04-14 19:20:47 +0200

    notification repo : added get notification

--- src/main/java/com/realestate/repositories/NotificationRepository.java
+++ src/main/java/com/realestate/repositories/NotificationRepository.java
@@ -18,5 +18,8 @@ public interface NotificationRepository extends JpaRepository<Notification, Inte
 	@Query(value = "INSERT INTO notification VALUES ( NULL, :id_appointement, :client_notif, :agent_notif, CURRENT_TIMESTAMP, '0', '0')", nativeQuery= true)
 	public int add_notification(@Param("id_appointement")int id_appointement, @Param("client_notif")String client_notif,  @Param("agent_notif")String agent_notif);
 
+	@Query(value = "SELECT * FROM `notification` WHERE id_appointement in ( SELECT id FROM appointement WHERE appointement.id_client=:id_client) ORDER BY time DESC", nativeQuery= true)
+	public List<Notification> get_notifications_by_client_id(@Param("id_client") int id_client);
+
 	
 }
