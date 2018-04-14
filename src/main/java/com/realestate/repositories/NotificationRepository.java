e84bb90f7f8e4854c5b4de54cd54b212aa13b8dc
AM77 <amine.griche77@gmail.com>
2018-04-14 19:19:11 +0200

    created notification repo

---
+++ src/main/java/com/realestate/repositories/NotificationRepository.java
@@ -0,0 +1,17 @@
+package com.realestate.repositories;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.realestate.models.Notification;
+
+public interface NotificationRepository extends JpaRepository<Notification, Integer>{
+
+	
+}
