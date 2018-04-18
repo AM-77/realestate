2eed229bae0436a257c97e0b4c681cca94db9c26
AM77 <amine.griche77@gmail.com>
2018-04-18 13:13:04 +0200

    appointement controller: added get reserve appointement method

--- src/main/java/com/realestate/controllers/AppointementController.java
+++ src/main/java/com/realestate/controllers/AppointementController.java
@@ -33,5 +33,350 @@ import com.realestate.services.ReportsService;
 @Controller
 public class AppointementController {
 	
+	@Autowired
+	private AppointementService appointementService;
+	@Autowired
+	private LodgementService lodgementService;
+	@Autowired
+	private ClientService clientService;
+	@Autowired
+	private AgentService agentService;
+	@Autowired
+	private NotificationService notificationService;
+	@Autowired
+	private ReportsService reportService;
+	
+	DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+	
+	@SuppressWarnings({ "deprecation" })
+	@GetMapping("reserve_appointement")
+	public String get_reserve_appointement(@RequestParam("id") int id_lodgement, HttpSession session,  Model model) throws ParseException {
+		
+		
+		
+		
+		if(session.getAttribute("client") != null ) {
+
+			if(((Client)session.getAttribute("client")).getUsername().equals("added_by_operator"))
+				return "redirect:/";
+			
+			model.addAttribute("client", session.getAttribute("client"));
+			
+			List<Notification_details> notifications = notificationService.get_notifications(session);
+			int notification_nbr = 0;
+			
+			for(Notification_details notif : notifications) {
+				if(!notif.isViewed())
+					notification_nbr ++ ;
+			}
+			
+			model.addAttribute("notification_nbr", notification_nbr);
+			model.addAttribute("notifications", notifications);
+			
+			
+			
+			Lodgement lodgement = lodgementService.get_lodgements_by_id(id_lodgement);
+			
+			if(lodgement == null) {
+				
+				session.setAttribute("looking_for", "lodgement");
+				return "redirect:/not_found_profile";
+			}
+		
+			List<Agent> agents = agentService.get_agents_by_locale(lodgement.getLocale());
+						
+			ArrayList<String> avail_dates = new ArrayList<String>();
+			
+			int day = new Date().getDate()+1;
+			int month = new Date().getMonth()+1;
+			int year = new Date().getYear() + 1900;
+
+			
+			if(( day == 32 && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12 )) || (day == 31 && (month == 4 || month == 6 || month == 9 || month == 10 || month == 11 )) || (day == 29 && month == 2)) {
+				System.out.println("cool");
+				day = 1;
+				
+				if(month == 12) {
+					month = 1;
+					year++;
+				}else {
+					month ++;
+				}
+			}else {
+				day ++;
+			}
+			
+			String __date = null;
+			Date date = null;
+			
+			
+			while(avail_dates.size() < 20) {
+				
+				if((day == 32 && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12 )) || (day == 31 && (month == 4 || month == 6 || month == 9 || month == 10 || month == 11 )) || (day == 29 && month == 2)) {
+					
+					day = 1;
+					
+					if(month == 12) {
+						month = 1;
+						year++;
+					}else {
+						month ++;
+					}
+				}else {
+					day ++;
+				}
+				
+				if(day<10) {
+					if(month<10)
+						__date = year+"-0"+month+"-0"+day;
+					else
+						__date = year+"-"+month+"-0"+day;
+				}else {
+					if(month<10)
+						__date = year+"-0"+month+"-"+day;
+					else
+						__date = year+"-"+month+"-"+day;
+				}
+				
+				date = df.parse(__date);
+								
+				if(appointementService.is_avail_date_first_half(date, id_lodgement) == true) {
+					avail_dates.add(__date + " f");
+				}
+					
+				if(appointementService.is_avail_date_second_half(date, id_lodgement) == true) {
+					avail_dates.add(__date + " s");
+				}				
+			
+			}
+			
+			List<Appointement_details> avail_appointements = new ArrayList<Appointement_details>();
+			String half;
+			
+			ArrayList<Agent> avail_agents = new ArrayList<>();
+			for(int i=0; i < avail_dates.size(); i++) {		
+				
+				Date _date = df.parse(avail_dates.get(i).substring(0,10));
+				String _half = avail_dates.get(i).substring(11,12);
+				avail_agents = new ArrayList<>();
+				
+				for(int j=0; j< agents.size(); j++) {
+					
+					
+					if(_half.equals("f")) {
+						
+						if(appointementService.is_avail_agent_first_half(_date, agents.get(j).getId())) {
+							avail_agents.add(agents.get(j));
+						}
+						
+					}else {
+						
+						if(appointementService.is_avail_agent_second_half(_date, agents.get(j).getId())) {
+							avail_agents.add(agents.get(j));
+						}
+					}
+						
+					if(avail_agents.size() == 25)
+						break;
+				}				
+				
+				if(_half.equals("f")) {
+					half= "Morning";
+				}else {
+					half="Evening";
+				}
+
+				avail_appointements.add(new Appointement_details(avail_dates.get(i).substring(0,10), half, avail_agents));
+			}
+
+					
+			model.addAttribute("avail_appointements", avail_appointements);
+			
+			if(session.getAttribute("type") != null && session.getAttribute("message") != null) {
+				model.addAttribute("type", session.getAttribute("type"));
+				model.addAttribute("message", session.getAttribute("message"));
+				
+				session.removeAttribute("type");
+				session.removeAttribute("message");
+			}
+			
+			String today;
+			
+			if((new Date().getDate()+1) < 10) {
+				if((new Date().getMonth()+1)<10)
+					today = (new Date().getYear()+1900) +"-0"+ (new Date().getMonth()+1) +"-0"+ (new Date().getDate()+1);
+				else
+					today = (new Date().getYear()+1900) +"-"+ (new Date().getMonth()+1) +"-0"+ (new Date().getDate()+1);
+			}else {
+				if((new Date().getMonth()+1) <10)
+					today = (new Date().getYear()+1900) +"-0"+ (new Date().getMonth()+1) +"-"+ (new Date().getDate()+1);
+				else
+					today = (new Date().getYear()+1900) +"-"+ (new Date().getMonth()+1) +"-"+ (new Date().getDate()+1);
+			}
+			
+			
+			
+			model.addAttribute("today", today);
+			model.addAttribute("avail_agents", avail_agents);
+			model.addAttribute("id_lodgement", id_lodgement);
+			
+			
+			return "appointement/new_appointement";	
+
+		}else {
+			
+			if(session.getAttribute("operator") != null) {
+				
+				model.addAttribute("operator", session.getAttribute("operator"));
+				
+				List<Notification_details> notifications = notificationService.get_notifications(session);
+				int notification_nbr = 0;
+				
+				for(Notification_details notif : notifications) {
+					if(!notif.isViewed())
+						notification_nbr ++ ;
+				}
+				
+				model.addAttribute("notification_nbr", notification_nbr);
+				model.addAttribute("notifications", notifications);
+				
+				Lodgement lodgement = lodgementService.get_lodgements_by_id(id_lodgement);
+				List<Agent> agents = agentService.get_agents_by_locale(lodgement.getLocale());
+							
+				ArrayList<String> avail_dates = new ArrayList<String>();
+				
+				int day = new Date().getDate()+1;
+				int month = new Date().getMonth()+1;
+				int year = new Date().getYear() + 1900;
+
+				String __date = null;
+				Date date = null;
+				while(avail_dates.size() < 20) {
+					if(day<10) {
+						if(month<10)
+							__date = year+"-0"+month+"-0"+day;
+						else
+							__date = year+"-"+month+"-0"+day;
+					}else {
+						if(month<10)
+							__date = year+"-0"+month+"-"+day;
+						else
+							__date = year+"-"+month+"-"+day;
+					}
+					
+					date = df.parse(__date);
+									
+					if(appointementService.is_avail_date_first_half(date, id_lodgement) == true) {
+						avail_dates.add(__date + " f");
+					}
+						
+					if(appointementService.is_avail_date_second_half(date, id_lodgement) == true) {
+						avail_dates.add(__date + " s");
+					}
+					
+					if((day == 31 && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12 )) || (day == 30 && (month == 4 || month == 6 || month == 9 || month == 10 || month == 11 )) || (day == 29 && month == 2)) {
+						day = 1;
+						
+						if(month == 12) {
+							month = 1;
+							year++;
+						}else {
+							month ++;
+						}
+					}else {
+						day ++;
+					}
+						
+				
+				}
+				
+				List<Appointement_details> avail_appointements = new ArrayList<Appointement_details>();
+				String half;
+				
+				ArrayList<Agent> avail_agents = new ArrayList<>();
+				for(int i=0; i < avail_dates.size(); i++) {		
+					
+					Date _date = df.parse(avail_dates.get(i).substring(0,10));
+					String _half = avail_dates.get(i).substring(11,12);
+					avail_agents = new ArrayList<>();
+					
+					for(int j=0; j< agents.size(); j++) {
+						
+						
+						if(_half.equals("f")) {
+							
+							if(appointementService.is_avail_agent_first_half(_date, agents.get(j).getId())) {
+								avail_agents.add(agents.get(j));
+							}
+							
+						}else {
+							
+							if(appointementService.is_avail_agent_second_half(_date, agents.get(j).getId())) {
+								avail_agents.add(agents.get(j));
+							}
+						}
+							
+						if(avail_agents.size() == 25)
+							break;
+					}				
+					
+					if(_half.equals("f")) {
+						half= "Morning";
+					}else {
+						half="Evening";
+					}
+
+					avail_appointements.add(new Appointement_details(avail_dates.get(i).substring(0,10), half, avail_agents));
+				}
+
+						
+				model.addAttribute("avail_appointements", avail_appointements);
+				
+				if(session.getAttribute("type") != null && session.getAttribute("message") != null) {
+					model.addAttribute("type", session.getAttribute("type"));
+					model.addAttribute("message", session.getAttribute("message"));
+					
+					session.removeAttribute("type");
+					session.removeAttribute("message");
+				}
+				
+				String today;
+				
+				if((new Date().getDate()+1) < 10) {
+					if((new Date().getMonth()+1)<10)
+						today = (new Date().getYear()+1900) +"-0"+ (new Date().getMonth()+1) +"-0"+ (new Date().getDate()+1);
+					else
+						today = (new Date().getYear()+1900) +"-"+ (new Date().getMonth()+1) +"-0"+ (new Date().getDate()+1);
+				}else {
+					if((new Date().getMonth()+1) <10)
+						today = (new Date().getYear()+1900) +"-0"+ (new Date().getMonth()+1) +"-"+ (new Date().getDate()+1);
+					else
+						today = (new Date().getYear()+1900) +"-"+ (new Date().getMonth()+1) +"-"+ (new Date().getDate()+1);
+				}
+				
+				model.addAttribute("today", today);
+				model.addAttribute("avail_agents", avail_agents);
+				model.addAttribute("id_lodgement", id_lodgement);
+				
+				return "appointement/new_operator_appointement";
+			
+			}else {
+				if(session.getAttribute("agent") != null || session.getAttribute("admin") != null) {
+					session.setAttribute("type", "error");
+					session.setAttribute("message", "Access denied.");
+					return "redirect:/";
+				}else {
+					session.setAttribute("type", "error");
+					session.setAttribute("message", "You have to login.");
+					session.setAttribute("url", "reserve_appointement?id="+id_lodgement);
+					return "redirect:/login";
+				}
+			}
+		}	
+		
+	}
+	
+	
+	
 	
 }
